8f4ebd7a9088128f0ac4e9ddaabf521091df9d79
kuwabara rikuya <riku@riku.local>
2017-06-28 17:05:55 +0900

    消し忘れ

--- app/models/time_card.rb
+++ app/models/time_card.rb
@@ -1,7 +1,7 @@
 class TimeCard < ApplicationRecord
   belongs_to :user
   def self.to_csv
-    CSV.generate(options) do |csv|
+    CSV.generate do |csv|
       csv << ["name","day","start","end"]
       all.each do |tc|
         csv << [tc.user.name, tc.work_date, tc.arrival_time, tc.leave_time]
